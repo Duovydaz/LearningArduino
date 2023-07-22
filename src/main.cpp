26ceed45831a68e725869f9584bb1fd4d509e9bc
Duovydaz <dovydaskubilius08@gmail.com>
2023-07-22 15:14:25 +0300

    added command

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
 #include <Arduino.h>
-
+#define LED1 13
 // put function declarations here:
-int myFunction(int, int); //offlinework
+// int myFunction(int, int);
 
-void setup() {
+void setup() { 
+  pinMode(A1,INPUT);
+  pinMode(LED1,OUTPUT);
+  digitalWrite(LED1,HIGH);
+  delay(2000);
+  digitalWrite(LED1,LOW);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.begin(9600);
-  Serial.println(result);
+  // int result = myFunction(2, 3);
+  // Serial.begin(9600);
+  // Serial.println(result);
 }
 
 void loop() {
+  delay(2000);
+  digitalWrite(LED1,HIGH);
+  delay(2000);
+  digitalWrite(LED1,LOW);
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
