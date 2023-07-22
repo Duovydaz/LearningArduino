fb2dbe844c499490283934f78068578c0a1212f9
Duovydaz <dovydaskubilius08@gmail.com>
2023-07-22 15:22:36 +0300

    added more leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #define LED1 13
+#define LED2 12
+#define LED3 11
+#define LED4 10
 // put function declarations here:
 // int myFunction(int, int);
 
 void setup() { 
   pinMode(A1,INPUT);
-  pinMode(LED1,OUTPUT);
-  digitalWrite(LED1,HIGH);
+  pinMode(LED2,OUTPUT);
+  digitalWrite(LED2,HIGH);
   delay(2000);
-  digitalWrite(LED1,LOW);
-  // put your setup code here, to run once:
+  digitalWrite(LED2,LOW);
   // int result = myFunction(2, 3);
   // Serial.begin(9600);
   // Serial.println(result);
@@ -17,9 +19,9 @@ void setup() {
 
 void loop() {
   delay(2000);
-  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
   delay(2000);
-  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
   // put your main code here, to run repeatedly:
 }
 
