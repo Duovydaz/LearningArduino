6e7dae32d84cd6f2f020c73cc939be6de0158e0b
Duovydaz <dovydaskubilius08@gmail.com>
2023-07-22 14:15:02 +0300

    added comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int); //activework
+int myFunction(int, int); //offlinework
 
 void setup() {
   // put your setup code here, to run once:
