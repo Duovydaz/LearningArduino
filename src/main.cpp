d1a5965e6dc6d97218a95dccab599237ddc129e3
Duovydaz <dovydaskubilius08@gmail.com>
2023-07-22 17:15:33 +0300

    added speaker

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,37 @@
 // int myFunction(int, int);
 
 void setup() { 
+  pinMode(3,OUTPUT);
   pinMode(A1,INPUT);
+  pinMode(LED1,OUTPUT);
   pinMode(LED2,OUTPUT);
-  digitalWrite(LED2,HIGH);
-  delay(2000);
-  digitalWrite(LED2,LOW);
+  pinMode(LED3,OUTPUT);
+  pinMode(LED4,OUTPUT);
   // int result = myFunction(2, 3);
   // Serial.begin(9600);
   // Serial.println(result);
 }
 
 void loop() {
-  delay(2000);
+  digitalWrite(3,HIGH);
+  delay(5000);
+  digitalWrite(3,LOW);
+  digitalWrite(LED1,HIGH);
+  delay(400);
+  digitalWrite(LED3,HIGH);
+  delay(300);
   digitalWrite(LED2,HIGH);
-  delay(2000);
+  delay(200);
+  digitalWrite(LED4,HIGH);
+  delay(100);
+  digitalWrite(LED1,LOW);
+  delay(400);
+  digitalWrite(LED3,LOW);
+  delay(300);
   digitalWrite(LED2,LOW);
+  delay(200);
+  digitalWrite(LED4,LOW);
+  delay(100);
   // put your main code here, to run repeatedly:
 }
 
