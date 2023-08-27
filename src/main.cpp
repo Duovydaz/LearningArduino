5b38e93e0aa20557086d449cfafeda8fa23f8ce7
Duovydaz <dovydaskubilius08@gmail.com>
2023-08-27 23:23:17 +0300

    Made another song

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #define LED2 12
 #define LED3 11
 #define LED4 10
+#define BTN1 A1
+#define BTN2 A2
+#define BTN3 A3
 // put function declarations here:
 // int myFunction(int, int);
 
 void setup() {
-  pinMode(3,OUTPUT);
   pinMode(A1,INPUT);
+  pinMode(3,OUTPUT);
   pinMode(LED1,OUTPUT);
   pinMode(LED2,OUTPUT);
   pinMode(LED3,OUTPUT);
@@ -19,48 +22,42 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(3,HIGH);
-  delay(150);
-  digitalWrite(3,LOW);
-  delay(150);
-  digitalWrite(3,HIGH);
-  delay(100);
-  digitalWrite(3,LOW);
-  delay(100);
-  digitalWrite(3,HIGH);
-  delay(100);
-  digitalWrite(3,LOW);
-  delay(100);
-  digitalWrite(3,HIGH);
-  delay(145);
-  digitalWrite(3,LOW);
-  delay(165);
-  digitalWrite(3,HIGH);
-  delay(250);
-  digitalWrite(3,LOW);
-  delay(500);
-  digitalWrite(3,HIGH);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(100);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(100);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(100);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(100);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(500);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(500);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(500);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
+delay(500);
+digitalWrite(3,HIGH);
+delay(100);
+digitalWrite(3,LOW);
 
-  digitalWrite(LED3,HIGH);
-  delay(150);
-  digitalWrite(LED3,LOW);
-  delay(150);
-  digitalWrite(LED2,HIGH);
-  delay(100);
-  digitalWrite(LED2,LOW);
-  delay(100);
-  digitalWrite(LED1,HIGH);
-  delay(100);
-  digitalWrite(LED1,LOW);
-  delay(100);
-  digitalWrite(LED1,HIGH);
-  delay(145);
-  digitalWrite(LED1,LOW);
-  delay(165);
-  digitalWrite(LED4,HIGH);
-  delay(250);
-  digitalWrite(LED4,LOW);
-  delay(500);
   // put your main code here, to run repeatedly:
 }
 
